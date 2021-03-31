737ea75f6111266d996fea817597e4df0f7f823d
JohnSmithW <salapin.danil1@mail.ru>
2021-04-01 02:46:10 +0300

    Added eslint and prettier config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "096de16c57d69e68371c";
+/******/ 	var hotCurrentHash = "44e141b3bf5fc815664f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
